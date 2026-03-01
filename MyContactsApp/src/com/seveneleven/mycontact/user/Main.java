27ad25b2b48f464f22cc97376dbaf5261b7d27fc
AkshatWalvekar <akshatwalvekar@gmail.com>
2026-03-01 17:16:11 +0530

    UC12-Implemented Assign Tags to Contacts Feature
    Removed an empty line for code cleanliness.

--- MyContactsApp/src/com/seveneleven/mycontact/user/Main.java
+++ MyContactsApp/src/com/seveneleven/mycontact/user/Main.java
@@ -8,7 +8,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 
-
 import com.seveneleven.mycontact.user.auth.BasicAuth;
 import com.seveneleven.mycontact.user.model.*;
 import com.seveneleven.mycontact.user.validation.*;
